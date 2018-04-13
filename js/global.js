4595eefa2429694fcc99292828a703cbb79b2013
Fernanda Assis <fernandaassis.dg@gmail.com>
2018-04-13 18:34:12 -0300

    Segunda Versão

--- js/global.js
+++ js/global.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+
+$(document).ready(function () {
 	var isDesktop = (function () {
 		return !('ontouchstart' in window) // works on most browsers 
 			|| !('onmsgesturechange' in window); // works on ie10
@@ -9,27 +10,26 @@ $(document).ready(function(){
 		$("#card-1").hover(function () {
 			$("#icon-1").toggleClass("ats-white");
 			$("#title-1").toggleClass("ats-white");
-		}); 
+		});
+		$("#card-2").hover(function () {
+			$("#icon-2").toggleClass("ats-white");
+			$("#title-2").toggleClass("ats-white");
+		});
+		$("#card-3").hover(function () {
+			$("#icon-3").toggleClass("ats-white");
+			$("#title-3").toggleClass("ats-white");
+		});
+		$("#card-4").hover(function () {
+			$("#icon-4").toggleClass("ats-white");
+			$("#title-4").toggleClass("ats-white");
+		});
+		$("#card-5").hover(function () {
+			$("#icon-5").toggleClass("ats-white");
+			$("#title-5").toggleClass("ats-white");
+		});
+		$("#card-6").hover(function () {
+			$("#icon-6").toggleClass("ats-white");
+			$("#title-6").toggleClass("ats-white");
+		});
 	}
-
-	$("#card-2").hover(function () {
-    	$("#icon-2").toggleClass("ats-white");
-    	$("#title-2").toggleClass("ats-white");
-	});
-	$("#card-3").hover(function () { 
-    	$("#icon-3").toggleClass("ats-white");
-    	$("#title-3").toggleClass("ats-white");
-	});
-	$("#card-4").hover(function () { 
-    	$("#icon-4").toggleClass("ats-white");
-    	$("#title-4").toggleClass("ats-white");
-	});
-    $("#card-5").hover(function () {
-        $("#icon-5").toggleClass("ats-white");
-        $("#title-5").toggleClass("ats-white");
-    });
-    $("#card-6").hover(function () { 
-        $("#icon-6").toggleClass("ats-white");
-        $("#title-6").toggleClass("ats-white");
-    });
 });
